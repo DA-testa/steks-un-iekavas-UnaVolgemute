d251a1b23ddedb3ecc5708edfc213ac738bed50a
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-02-19 20:24:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
             
