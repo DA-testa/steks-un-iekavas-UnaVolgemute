1850da2a95bd05693026201c88a1b092d31f5091
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-02-19 20:29:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def find_mismatch(text):
                 return i+1
             opening_brackets_stack.pop()
             
-     if opening_brackets_stack:
-        return opening_brackets_stack[0].position
-    return "Success"
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
+        return "Success"
 
 
 def main():
