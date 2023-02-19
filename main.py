73733060e7b2690295cb2eef7d800f68e66513ca
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-02-19 17:52:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i+1));
-            pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
@@ -26,7 +25,6 @@ def find_mismatch(text):
         if opening_brackets_stack:
             return opening_brackets_stack[0].position
         return "Success"
-        pass
 
 
 def main():
