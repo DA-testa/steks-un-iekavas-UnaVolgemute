3de22628ce3865f528036eeed49ac44c901bd683
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-02-19 17:10:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def find_mismatch(text):
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-          opening_brackets_stack.pop()
         if opening_brackets_stack:
           return opening_brackets_stack[0].position
         return "Success"
